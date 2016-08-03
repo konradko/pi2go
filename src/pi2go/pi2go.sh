91316f2f2a51792ab7db84498b7fb9ba9b61362d
konradko <konrad.korzel@gmail.com>
2016-08-03 15:07:26 +0100

    Add pi2go lib source code

---
+++ src/pi2go/pi2go.sh
@@ -0,0 +1,19 @@
+wget -q http://4tronix.co.uk/pi2go/servod.xxx -O servod
+wget -q http://4tronix.co.uk/pi2go/pi2go.py -O pi2go.py
+wget -q http://4tronix.co.uk/pi2go/motorTest.py -O motorTest.py
+wget -q http://4tronix.co.uk/pi2go/IRTest.py -O IRTest.py
+wget -q http://4tronix.co.uk/pi2go/servoTest.py -O servoTest.py
+wget -q http://4tronix.co.uk/pi2go/sonarTest.py -O sonarTest.py
+wget -q http://4tronix.co.uk/pi2go/LEDTest.py -O LEDTest.py
+wget -q http://4tronix.co.uk/pi2go/lightTest.py -O lightTest.py
+wget -q http://4tronix.co.uk/pi2go/lineTest.py -O lineTest.py
+wget -q http://4tronix.co.uk/pi2go/liteTest.py -O liteTest.py
+wget -q http://4tronix.co.uk/pi2go/switchTest.py -O switchTest.py
+wget -q http://4tronix.co.uk/pi2go/testLEDs.py -O testLEDs.py
+wget -q http://4tronix.co.uk/pi2go/beebot.py -O beebot.py
+wget -q http://4tronix.co.uk/pi2go/beebot1.py -O beebot1.py
+
+wget -q http://4tronix.co.uk/pi2go/pca9685.py -O pca9685.py
+wget -q http://4tronix.co.uk/pi2go/sgh_PCF8591P.py -O sgh_PCF8591P.py
+
+chmod +x servod
